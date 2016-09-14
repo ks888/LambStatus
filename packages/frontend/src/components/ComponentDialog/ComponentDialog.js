911920d7585077df7f4e3447ad0425ae17467bd2
Kishin Yagami <k.yagami.suou@gmail.com>
2016-09-15 00:09:17 +0900

    Fix js effect does not work issue

--- packages/frontend/src/components/ComponentDialog/ComponentDialog.js
+++ packages/frontend/src/components/ComponentDialog/ComponentDialog.js
@@ -1,18 +1,44 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import classnames from 'classnames'
+import classes from './ComponentDialog.scss'
 
 class ComponentDialog extends React.Component {
+  componentDidMount () {
+    let jsElems = [
+      ReactDOM.findDOMNode(this.refs.textfield_name),
+      ReactDOM.findDOMNode(this.refs.textfield_desc),
+      ReactDOM.findDOMNode(this.refs.button_complete),
+      ReactDOM.findDOMNode(this.refs.button_cancel)
+    ]
+    jsElems.forEach((jsElem) => {
+      componentHandler.upgradeElement(jsElem)
+    })
+  }
+
   render () {
-    return (<dialog className='mdl-dialog'>
-      <h4 className='mdl-dialog__title'>
+    return (<dialog className={classnames('mdl-dialog', classes.dialog)}>
+      <h2 className={classnames('mdl-dialog__title', classes.title)}>
         <i className='material-icons'>add</i>
-        Component
-      </h4>
+        COMPONENT
+      </h2>
       <div className='mdl-dialog__content'>
-        Comopnent Name
+        <div className={classnames('mdl-textfield', 'mdl-js-textfield', 'mdl-textfield--floating-label',
+          classes.dialog)} ref='textfield_name'>
+          <input className='mdl-textfield__input' type='text' id='name' />
+          <label className='mdl-textfield__label' htmlFor='name'>Name</label>
+        </div>
+        <div className={classnames('mdl-textfield', 'mdl-js-textfield', 'mdl-textfield--floating-label',
+          classes.dialog)} ref='textfield_desc'>
+          <textarea className='mdl-textfield__input' type='text' rows='2' id='desc' />
+          <label className='mdl-textfield__label' htmlFor='desc'>Description</label>
+        </div>
       </div>
       <div className='mdl-dialog__actions'>
-        <button type='button' className='mdl-button mdl-js-button mdl-button--raised mdl-button--accent' onClick={this.props.onCompleted}>Add</button>
-        <button type='button' className='mdl-button mdl-js-button mdl-button--raised' onClick={this.props.onCanceled}>Cancel</button>
+        <button type='button' className='mdl-button mdl-js-button mdl-button--raised
+          mdl-button--accent' onClick={this.props.onCompleted} ref='button_complete'>Add</button>
+        <button type='button' className='mdl-button mdl-js-button mdl-button--raised'
+          onClick={this.props.onCanceled} ref='button_cancel'>Cancel</button>
       </div>
     </dialog>)
   }
