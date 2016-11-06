23e6702a4733fb8c916505940a8b2e2902ade7bc
Kishin Yagami <k.yagami.suou@gmail.com>
2016-11-06 17:10:58 +0900

    Remove unused utils

--- utils/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config({path: `${__dirname}/../.env`})
-
-let key = process.argv[2]
-console.log(process.env[key])
--- utils/config.sh
+++
@@ -1,7 +0,0 @@
-
-config() {
-  KEY=$1
-  CONFIG_JS="$(dirname "${BASH_SOURCE:-$0}")/config.js"
-
-  babel-node ${CONFIG_JS} ${KEY}
-}
