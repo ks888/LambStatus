eb11ec34799c3d9cbee531df1514dfd3fd99e40f
Kishin Yagami <k.yagami.suou@gmail.com>
2016-11-14 19:36:21 +0900

    Add lambci-ecs setting

--- .lambci.js
+++ .lambci.js
@@ -1,4 +1,8 @@
 module.exports = {
   cmd: 'cd packages/frontend && npm install && npm run test',
-  build: true
+  build: true,
+  docker: {
+    cluster: 'lambci-ecs-Cluster-7V5M1CB50WP1',
+    task: 'lambci-ecs-BuildTask-1ATBBSYTY0L2M',
+  }
 }
