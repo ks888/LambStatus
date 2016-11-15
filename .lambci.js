c41bce10b2fe460342dbd467626e13253e257f4e
Kishin Yagami <k.yagami.suou@gmail.com>
2016-11-15 20:26:14 +0900

    Fix branches setting

--- .lambci.js
+++ .lambci.js
@@ -1,10 +1,7 @@
 module.exports = {
   cmd: 'cd packages/frontend && npm install && npm run test',
   build: true,
-  branches: {
-    master: true,
-    lambci: true
-  },
+  branches: true,
   docker: {
     cluster: 'lambci-ecs-Cluster-7V5M1CB50WP1',
     task: 'lambci-ecs-BuildTask-1ATBBSYTY0L2M',
