f7e86f3c923036180e2cf156ce04bee4bf61ce35
Kishin Yagami <k.yagami.suou@gmail.com>
2016-11-15 22:10:53 +0900

    Fix not quoted bug

--- .lambci.js
+++ .lambci.js
@@ -7,7 +7,7 @@ module.exports = {
     task: 'lambci-ecs-BuildTask-1ATBBSYTY0L2M',
   },
   env: {
-    LAMBCI_REPO: LambStatus
+    LAMBCI_REPO: 'LambStatus'
   },
   allowConfigOverrides: false
 }
