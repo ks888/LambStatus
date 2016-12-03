d6d9f140cea16d94cb768c2544da551bed39c065
Kishin Yagami <k.yagami.suou@gmail.com>
2016-12-03 21:49:36 +0900

    Add signin action

--- packages/frontend/src/actions/users.js
+++ packages/frontend/src/actions/users.js
@@ -1,34 +1,84 @@
 import 'whatwg-fetch'
+import { CognitoUserPool, AuthenticationDetails, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js'
 import { checkStatus } from 'utils/fetch'
 import { apiURL } from 'utils/settings'
 
-export const SIGNIN = 'SIGNIN'
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS'
 
-export function signin (token) {
+export function signinSuccess (token) {
   return {
-    type: SIGNIN,
+    type: SIGNIN_SUCCESS,
     token
   }
 }
 
-export const fetchIncidents = (callbacks = {}) => {
-  const { onLoad, onSuccess, onFailure } = callbacks
-  if (true) return
+export const signin = (username, password, callbacks = {}) => {
+  const { onLoad, onNewPasswordRequested, onSuccess, onFailure } = callbacks
   return dispatch => {
     if (onLoad && typeof onLoad === 'function') onLoad()
-    return fetch(apiURL + 'signin')
-      .then(checkStatus)
-      .then(response => response.json())
-      .then(token => {
+    const authenticationData = {
+      Username: username,
+      Password: password
+    }
+    const authenticationDetails = new AuthenticationDetails(authenticationData)
+
+    const poolData = {
+      UserPoolId: 'ap-northeast-1_BTtTftreC',
+      ClientId: '2t48m7pdb91l11fuo6t1u9ngkl'
+    }
+    const userPool = new CognitoUserPool(poolData)
+    const userData = {
+      Username: username,
+      Pool: userPool
+    }
+    const cognitoUser = new CognitoUser(userData)
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => {
+        console.log('JWT token: ' + result.getAccessToken().getJwtToken())
         if (onSuccess && typeof onSuccess === 'function') onSuccess()
-        dispatch(signin(token))
-      })
-      .catch(error => {
+        // dispatch(signin(token))
+      },
+      onFailure: (error) => {
         console.error(error.message)
         console.error(error.stack)
-        if (error.name === 'HTTPError') {
-          if (onFailure && typeof onFailure === 'function') onFailure(error.message)
+        if (onFailure && typeof onFailure === 'function') onFailure(error.message)
+      },
+      newPasswordRequired: () => {
+        if (!onNewPasswordRequested || !(typeof onNewPasswordRequested === 'function')) {
+          console.error('onNewPasswordRequested callback is not implemented')
+          if (onFailure && typeof onFailure === 'function') {
+            onFailure('Internal error. Please contact your admin.')
+          }
+          return
         }
-      })
+
+        onNewPasswordRequested((newPassword, callbacks) => {
+          setNewPassword(cognitoUser, newPassword, callbacks)
+        })
+      }
+    })
   }
+}
+
+const setNewPassword = (cognitoUser, newPassword, callbacks = {}) => {
+  const { onLoad, onSuccess, onFailure } = callbacks
+  if (onLoad && typeof onLoad === 'function') onLoad()
+
+  cognitoUser.completeNewPasswordChallenge(newPassword, {}, {
+    onSuccess: (result) => {
+      console.log('JWT token: ' + result.getAccessToken().getJwtToken())
+      if (onSuccess && typeof onSuccess === 'function') onSuccess()
+      // dispatch(signin(token))
+    },
+    onFailure: (error) => {
+      if (error instanceof Error) {
+        console.error(error.message)
+        console.error(error.stack)
+        if (onFailure && typeof onFailure === 'function') onFailure(error.message)
+      } else {
+        console.error(error)
+        if (onFailure && typeof onFailure === 'function') onFailure(error)
+      }
+    }
+  })
 }
--- packages/frontend/src/components/Signin/Signin.js
+++ packages/frontend/src/components/Signin/Signin.js
@@ -5,6 +5,11 @@ import Button from 'components/Button'
 import TextField from 'components/TextField'
 import classes from './Signin.scss'
 
+const dialogType = {
+  signin: 1,
+  setNewPassword: 2
+}
+
 export default class Signin extends React.Component {
   static propTypes = {
     signin: PropTypes.func.isRequired
@@ -13,9 +18,11 @@ export default class Signin extends React.Component {
   constructor () {
     super()
     this.state = {
-      email: '',
+      dialogType: dialogType.signin,
+      username: '',
       password: '',
-      isUpdating: false
+      isUpdating: false,
+      newPasswordCallback: null
     }
   }
 
@@ -32,25 +39,46 @@ export default class Signin extends React.Component {
     }
   }
 
-  handleChangeEmail = (value) => {
-    this.setState({email: value})
+  handleChangeUsername = (value) => {
+    this.setState({username: value})
   }
 
   handleChangePassword = (value) => {
     this.setState({password: value})
   }
 
-  handleClickDoneButton = (e) => {
-    this.props.signin(this.state.email, this.state.password, {
-      onLoad: () => { this.setState({isUpdating: true}) },
-      onSuccess: () => {
-        this.setState({isUpdating: false})
-        this.handleHideDialog()
-      },
-      onFailure: (msg) => {
-        this.setState({isUpdating: false, message: msg})
+  updateCallbacks = {
+    onLoad: () => { this.setState({isUpdating: true}) },
+    onSuccess: () => {
+      this.setState({isUpdating: false})
+      this.handleHideDialog()
+    },
+    onFailure: (msg) => {
+      this.setState({isUpdating: false, message: msg})
+    }
+  }
+
+  handleClickSigninButton = (e) => {
+    this.props.signin(this.state.username, this.state.password, {
+      ...this.updateCallbacks,
+      onNewPasswordRequested: (callback) => {
+        this.setState({
+          isUpdating: false,
+          password: '',
+          dialogType: dialogType.setNewPassword,
+          newPasswordCallback: callback
+        })
       }
     })
+  }
+
+  handleSetNewPasswordButton = (e) => {
+    const { password, newPasswordCallback } = this.state
+    if (newPasswordCallback && typeof newPasswordCallback === 'function') {
+      newPasswordCallback(password, this.updateCallbacks)
+    } else {
+      console.warn('States do not include newPasswordCallback')
+    }
   }
 
   handleHideDialog = () => {
@@ -61,20 +89,46 @@ export default class Signin extends React.Component {
     }
   }
 
-  renderDialog = () => {
+  renderSigninDialog = () => {
     return (<dialog className={classnames('mdl-dialog', classes.dialog)} ref='dialog'>
       <h2 className={classnames('mdl-dialog__title', classes.title)}>
         Signin
       </h2>
       <div className='mdl-dialog__content'>
-        <TextField label='Email' text={this.state.email} rows={1} onChange={this.handleChangeEmail} />
+        <TextField label='Username' text={this.state.username} rows={1} onChange={this.handleChangeUsername} />
         <TextField label='Password' text={this.state.password} rows={1} onChange={this.handleChangePassword} />
       </div>
       <div className='mdl-dialog__actions'>
-        <Button onClick={this.handleClickDoneButton} name='Signin'
+        <Button onClick={this.handleClickSigninButton} name='Signin'
           class='mdl-button--accent' disabled={this.state.isUpdating} />
       </div>
     </dialog>)
+  }
+
+  renderNewPasswordDialog = () => {
+    return (<dialog className={classnames('mdl-dialog', classes.dialog)} ref='dialog'>
+      <h2 className={classnames('mdl-dialog__title', classes.title)}>
+        Signin
+      </h2>
+      <div className='mdl-dialog__content'>
+        <TextField label='New Password' text={this.state.password} rows={1} onChange={this.handleChangePassword} />
+      </div>
+      <div className='mdl-dialog__actions'>
+        <Button onClick={this.handleSetNewPasswordButton} name='DONE'
+          class='mdl-button--accent' disabled={this.state.isUpdating} />
+      </div>
+    </dialog>)
+  }
+
+  renderDialog = () => {
+    switch (this.state.dialogType) {
+      case dialogType.signin:
+        return this.renderSigninDialog()
+      case dialogType.setNewPassword:
+        return this.renderNewPasswordDialog()
+      default:
+        console.warn('unknown dialog type: ', this.state.dialogType)
+    }
   }
 
   render () {
