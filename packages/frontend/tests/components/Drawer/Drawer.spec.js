e4fce149558722c75d350b8146a874a2078c947f
Kishin Yagami <k.yagami.suou@gmail.com>
2017-04-29 19:10:51 +0900

    Fix tests

--- packages/frontend/tests/components/Drawer/Drawer.spec.js
+++ packages/frontend/tests/components/Drawer/Drawer.spec.js
@@ -10,24 +10,10 @@ describe('(Component) Drawer', () => {
     _wrapper = shallow(<Drawer />)
   })
 
-  it('Should render a Link to Incidents route', () => {
-    const incidentNode = _wrapper.find(Link).at(0)
-    assert(incidentNode.key() === 'incidents')
-    assert(incidentNode.childAt(0).text() === 'Incidents')
-    assert(incidentNode.prop('to') === '/incidents')
-  })
-
-  it('Should render a Link to Maintenances route', () => {
-    const maintenanceNode = _wrapper.find(Link).at(1)
-    assert(maintenanceNode.key() === 'maintenances')
-    assert(maintenanceNode.childAt(0).text() === 'Scheduled Maintenances')
-    assert(maintenanceNode.prop('to') === '/maintenances')
-  })
-
   it('Should render a Link to Components route', () => {
-    const componentNode = _wrapper.find(Link).at(2)
-    assert(componentNode.key() === 'components')
-    assert(componentNode.childAt(0).text() === 'Components')
-    assert(componentNode.prop('to') === '/components')
+    const incidentNode = _wrapper.find(Link).at(0)
+    assert(incidentNode.key() === 'components')
+    assert(incidentNode.childAt(0).text() === 'Components')
+    assert(incidentNode.prop('to') === '/components')
   })
 })
