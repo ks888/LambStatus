d18734b6d9e9884664637e209a81f0ab7f1a408e
Kishin Yagami <k.yagami.suou@gmail.com>
2017-06-07 21:00:33 +0900

    Fix sometimes function is not deployed issue

--- packages/lambda/bin/deploy.js
+++ packages/lambda/bin/deploy.js
@@ -1,35 +1,57 @@
 import fs from 'fs'
 import path from 'path'
 import dotenv from 'dotenv'
-import { execSync } from 'child_process'
+import { exec } from 'child_process'
 
 dotenv.config({path: `${__dirname}/../../../.env`})
 
 const buildDir = path.normalize(`${__dirname}/../build`)
 const funcsDir = path.normalize(`${buildDir}/functions`)
 
-try {
+const deploy = (cmd) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, { cwd: buildDir, maxBuffer: 100 * 1024 * 1024, timeout: 10 * 1000 }, (error, stdout, stderr) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      if (stdout.toString().length === 0 && stderr.toString().length === 0) {
+        // If the output is empty, it is doubtful the deploy was really successful
+        reject(new Error('apex exited successfully but the output is suspicious'))
+        return
+      }
+      resolve()
+    })
+  })
+}
+
+const deployAll = async () => {
   const funcs = fs.readdirSync(funcsDir)
-  funcs.forEach(funcName => {
-    const cmd = `apex deploy ${funcName}`
+  for (let i = 0; i < funcs.length; i++) {
+    const func = funcs[i]
+    const cmd = `apex deploy ${func}`
     const numRetries = 3
-    let i
-    for (i = 0; i < numRetries; i++) {
+    let j
+    for (j = 0; j < numRetries; j++) {
       try {
         // sometimes 'apex deploy' returns error
-        execSync(cmd, { cwd: buildDir, maxBuffer: 100 * 1024 * 1024, timeout: 10 * 1000 })
-        console.log(`deployed ${funcName}`)
+        await deploy(cmd)
+        console.log(`deployed ${func}`)
         break
-      } catch (error) {
+      } catch (err) {
+        console.log(err)
         console.log('retry...')
       }
     }
 
-    if (i === numRetries) {
-      throw new Error(`failed to deploy ${funcName}`)
+    if (j === numRetries) {
+      throw new Error(`failed to deploy ${func}`)
     }
-  })
-} catch (error) {
-  console.log(error.message)
-  console.log(error.stack)
+  }
 }
+
+deployAll().then(() => {
+  console.log('deployed all functions')
+}).catch(err => {
+  console.log(err)
+})
