e0cb3989f5c5e69191d7b0a4e928503df8496aef
Kishin Yagami <k.yagami.suou@gmail.com>
2017-06-11 19:16:25 +0900

    Use istanbul-instrumenter-loader instead of insparta-loader, which is deprecated

--- packages/frontend/config/index.js
+++ packages/frontend/config/index.js
@@ -57,10 +57,7 @@ export default function () {
     // ----------------------------------
     // Test Configuration
     // ----------------------------------
-    coverage_reporters : [
-      { type : 'text' },
-      { type : 'lcov', dir : 'coverage' }
-    ]
+    coverage_reporters : ['text', 'lcov']
   }
 
   /************************************************
