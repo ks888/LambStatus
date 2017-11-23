79c1bcd2acccf26d640374a88f574384b0cf941b
Kishin Yagami <k.yagami.suou@gmail.com>
2017-11-23 18:49:31 +0900

    Use given admin page url for cognito pool setting

--- packages/lambda/src/api/cognitoCreateUserPool/index.js
+++ packages/lambda/src/api/cognitoCreateUserPool/index.js
@@ -2,32 +2,12 @@ import response from 'cfn-response'
 import Cognito, { UserPool } from 'aws/cognito'
 import { SettingsProxy } from 'api/utils'
 
-export async function handle (event, context, callback) {
+const createUserPool = async (event, context, poolID) => {
   const {
     PoolName: poolName,
     AdminPageURL: adminPageURL,
     SnsCallerArn: snsCallerArn
   } = event.ResourceProperties
-
-  if (event.RequestType === 'Delete') {
-    try {
-      const poolID = event.PhysicalResourceId
-      await new Cognito().deleteUserPool(poolID)
-      response.send(event, context, response.SUCCESS, {UserPoolID: poolID}, poolID)
-    } catch (error) {
-      console.log(error.message)
-      console.log(error.stack)
-      response.send(event, context, response.FAILED)
-    }
-    return
-  }
-
-  if (event.RequestType === 'Update') {
-    const poolID = event.PhysicalResourceId
-    // If `physicalResourceId` is changed, the custom resource will be deleted.
-    response.send(event, context, response.SUCCESS, {UserPoolID: poolID}, poolID)
-    return
-  }
 
   try {
     const settings = new SettingsProxy()
@@ -35,11 +15,59 @@ export async function handle (event, context, callback) {
 
     const userPool = new UserPool({userPoolName: poolName, serviceName, adminPageURL, snsCallerArn})
     const createdUserPool = await new Cognito().createUserPool(userPool)
+
     const poolID = createdUserPool.userPoolID
     response.send(event, context, response.SUCCESS, {UserPoolID: poolID}, poolID)
   } catch (error) {
     console.log(error.message)
     console.log(error.stack)
     response.send(event, context, response.FAILED)
+  }
+}
+
+const updateUserPool = async (event, context) => {
+  const {
+    AdminPageURL: adminPageURL
+  } = event.ResourceProperties
+  const poolID = event.PhysicalResourceId
+
+  const cognito = new Cognito()
+  const userPool = await cognito.getUserPool(poolID)
+  const settings = new SettingsProxy()
+  userPool.serviceName = await settings.getServiceName()
+  userPool.adminPageURL = adminPageURL
+  await cognito.updateUserPool(userPool)
+
+  // Note: If `physicalResourceId` is changed, the custom resource will be deleted.
+  response.send(event, context, response.SUCCESS, {UserPoolID: poolID}, poolID)
+}
+
+const deleteUserPool = async (event, context) => {
+  const poolID = event.PhysicalResourceId
+
+  try {
+    await new Cognito().deleteUserPool(poolID)
+    response.send(event, context, response.SUCCESS, {UserPoolID: poolID}, poolID)
+  } catch (error) {
+    console.log(error.message)
+    console.log(error.stack)
+    response.send(event, context, response.FAILED)
+  }
+}
+
+export async function handle (event, context, callback) {
+  switch (event.RequestType) {
+    case 'Create':
+      await createUserPool(event, context)
+      break
+    case 'Update':
+      await updateUserPool(event, context)
+      break
+    case 'Delete':
+      await deleteUserPool(event, context)
+      break
+    default:
+      console.log('unknown request type:', event.RequestType)
+      response.send(event, context, response.FAILED)
   }
 }
