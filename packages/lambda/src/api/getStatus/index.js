c7590604f0f06d6dc6ec6ec68756860d659b082c
rosszhan <rosszhan@synology.com>
2018-03-22 10:59:41 +0800

    Fix GetStatus/index.js error

--- packages/lambda/src/api/getStatus/index.js
+++ packages/lambda/src/api/getStatus/index.js
@@ -3,18 +3,17 @@ import ComponentsStore from 'db/components'
 export async function handle (event, context, callback) {
   try {
     let comps = await new ComponentsStore().query()
-	var downComp = []
-	comps.forEach(function(comp) {
-		if(comp.status == 'Major Outage') {
-			downComp.push(comp.name)
-		}		
-	})	
-		
-	let stat = { 'status': (downComp.length > 0 ? 'Major Outage' : 'Operational'), 'componentOutages': downComp } 
+    var downComp = []
+    comps.forEach(function (comp) {
+      if (comp.status === 'Major Outage') {
+        downComp.push(comp.name)
+      }
+    })
+    let stat = { 'status': (downComp.length > 0 ? 'Major Outage' : 'Operational'), 'componentOutages': downComp }
     callback(null, stat)
   } catch (error) {
     console.log(error.message)
     console.log(error.stack)
-	callback('Error: failed to get current status')
+    callback('Error: failed to get current status')
   }
 }
