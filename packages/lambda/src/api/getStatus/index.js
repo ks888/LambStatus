36944843c204746669dc99f69360bd38027e4380
rosszhan <rosszhan@synology.com>
2018-03-26 14:04:43 +0800

    modify getStatus API.
    Now you can get "Operational", "Major Outage", "Partial Outage", "Degraded Performance", "Under Maintenance".

--- packages/lambda/src/api/getStatus/index.js
+++ packages/lambda/src/api/getStatus/index.js
@@ -4,12 +4,20 @@ export async function handle (event, context, callback) {
   try {
     let comps = await new ComponentsStore().query()
     var downComp = []
-    comps.forEach(function (comp) {
-      if (comp.status === 'Major Outage') {
-        downComp.push(comp.name)
-      }
+    var compNum = comps.length
+    let stat = { 'status': 'Operational' }
+    comps.forEach(function(comp) {
+        if (comp.status === 'Major Outage' || comp.status === 'Partial Outage') {
+            if (comp.status === 'Major Outage') downComp.push(comp)
+            stat = { 'status': (downComp.length < compNum ? 'Partial Outage' : 'Major Outage') }
+        } else if (downComp.length === 0) {
+            if (stat.status !== 'Degraded Performance' && comp.status === 'Under Maintenance') {
+                stat = { 'status': 'Under Maintenance' }
+            } else if (comp.status === 'Degraded Performance') {
+                stat = { 'status': 'Degraded Performance' }
+            }
+        }
     })
-    let stat = { 'status': (downComp.length > 0 ? 'Major Outage' : 'Operational'), 'componentOutages': downComp }
     callback(null, stat)
   } catch (error) {
     console.log(error.message)
