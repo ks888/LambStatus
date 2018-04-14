b89b61b94f9ebcb77a4c21999c41056b5ba6d2eb
Kishin Yagami <k.yagami.suou@gmail.com>
2018-04-14 18:27:37 +0900

    Show timezone at the top of page

--- packages/frontend/src/components/statusPage/Header/index.js
+++ packages/frontend/src/components/statusPage/Header/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Title from 'components/statusPage/Title'
 import SubscriptionMenu from 'components/statusPage/SubscriptionMenu'
+import { currTimeZone } from 'utils/datetime'
 import classes from './Header.scss'
 
 export default class Header extends React.Component {
   render () {
     return (
-      <div className={classes.top}>
-        <Title />
-        <SubscriptionMenu />
+      <div>
+        <div className={classes.top}>
+          <Title />
+          <SubscriptionMenu />
+        </div>
+        <div className={classes.timezone}>All times are in: {currTimeZone}</div>
       </div>
     )
   }
--- packages/frontend/src/utils/datetime.js
+++ packages/frontend/src/utils/datetime.js
@@ -1,5 +1,7 @@
 import moment from 'moment-timezone'
 
+export const currTimeZone = moment().tz(moment.tz.guess()).format('z')
+
 export const getDateTime = (datetime) => {
   return moment.tz(datetime, moment.tz.guess())
 }
